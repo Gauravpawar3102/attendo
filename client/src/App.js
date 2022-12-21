ca6a9474b5ef13ebdaf6dd32ce33fdb9779d53e7
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-12-21 21:22:37 +0530

    ....

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FormInputPage from './pages/FormInputPage';
 import HomePage from './pages/HomePage';
 import Dashboard from './pages/Dashboard';
+import ClassDetails from './pages/ClassDetails';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<FormInputPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/classdetails" element={<ClassDetails />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/ClassDetails.jsx
+++ client/src/pages/ClassDetails.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 function ClassDetails() {
-  return <div>ClassDetails</div>;
+  return (
+    <div className="">
+      <table>
+        <thead>
+          <tr>
+            <th>asdfsda</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>dsfasd</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default ClassDetails;
