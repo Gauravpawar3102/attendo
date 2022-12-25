c524d5c1a7184d8c826bae59756b7b768e44aa7f
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-12-25 13:10:24 +0530

    done

--- client/src/components/DashboardCard.jsx
+++ client/src/components/DashboardCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function DashboardCard() {
+function DashboardCard({ name }) {
   return (
     <div className="dashboard-card-container bg-blue-500 w-40 p-2 rounded-lg ">
       <div className="top  text-white my-2">
-        <div className="class text-lg font-medium">CSE-B</div>
+        <div className="class text-lg font-medium">{name}</div>
         <div className="class text-sm font-semibold">Teacher : Rajesh Sir</div>
       </div>
       <div className="bottom flex justify-center">
@@ -12,6 +12,11 @@ function DashboardCard() {
           Open
         </button>
       </div>
+      {/* {console.log({
+        data,
+        error,
+        loading,
+      })} */}
     </div>
   );
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import DashBoardNavbar from '../components/DashBoardNavbar';
 import DashboardCard from '../components/DashboardCard';
+import { gql, useQuery } from '@apollo/client';
 
+const GET_CLASSES = gql`
+  query {
+    classes {
+      name
+    }
+  }
+`;
 function Dashboard() {
+  const { data, error, loading } = useQuery(GET_CLASSES);
+  if (error) return <div> Something went wrong</div>;
+
+  if (loading) return <div>Loading...</div>;
+
+  const { classes } = data;
+
   return (
     <div className="top-container flex flex-col items-center justify-center ">
       <DashBoardNavbar />
       <div className="container w-4/5 flex flex-wrap gap-6 mt-5">
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
+        {
+          classes.map((clas) => {
+            return <DashboardCard name={clas.name} />;
+          })
+
+          // console.log(data)
+        }
       </div>
     </div>
   );
